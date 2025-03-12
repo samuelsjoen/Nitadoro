c21bace17315f94d1b5753def359bbe53b42f674
samuelsjoen <122876089+samuelsjoen@users.noreply.github.com>
2025-03-12 16:04:44 +0100

    Implemented the basic timer logic and a simple UI

---
+++ nitadoro.client/src/Timer.js
@@ -0,0 +1,85 @@
+/**
+ * A timer object for counting down time
+ */
+class Timer {
+    /**
+     * @param {*} ring The function to be executed once timer finishes
+     */
+    constructor(ring, update) {
+        this.counting = false;
+        this.timerCount = null;
+        this.timerId = null;
+        this.ring = ring;
+        this.update = update;
+    }
+
+    /** Starts the timer */
+    startTimer() {
+        if (this.counting) {
+            throw new Error("Timer already started");
+        }
+        if (!this.timerCount) {
+            throw new Error("Timer duration is not set");
+        }
+        this.counting = true;
+
+        this.timerId = setInterval(() => {
+                this.timerCount--;
+                this.update(this.timerCount);
+                if (this.timerCount === 0) {
+                    this.counting = false;
+                    clearInterval(this.timerId);
+                    this.ring();
+                }
+            }, 1000);
+    }
+
+    /**
+     * Pauses the timer if running
+     * @throws {Error} if timer is currently not running
+     */
+    pauseTimer() {
+        if (this.counting && this.timerCount != 0) {
+            clearInterval(this.timerId);
+            this.counting = false;
+        } else {
+            throw new Error("Unable to pause timer, no timer currently running");
+        }
+    }
+
+    /**
+     * Resumes the timer if it's paused
+     * @throws {Error} if timer is already running
+     */
+    resumeTimer() {
+        if (!this.counting && this.timerCount > 0) {
+            this.startTimer();
+        } else {
+            throw new Error("Unable to resume timer, timer already running");
+        }
+    }
+
+    /**
+     * @returns the current time of the timer
+     */
+    get time() {
+        return this.timerCount;
+    }
+
+    /**
+     * Sets the time of the timer
+     */
+    set time(time) {
+        this.timerCount=time;
+        this.update(this.timerCount);
+    }
+
+    /**
+     * @returns a bool describing wether the timer is running or not
+     */
+    get counting() {
+        return this.counting;
+    }
+}
+
+export default Timer;
